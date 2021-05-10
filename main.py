a379b482601aa747cf3bb89779123c1adafe9375
Mostafa Tawfiq <mostafa.mohammad.twfiq@gmail.com>
2021-05-11 00:18:11 +0200

    Implemented quick sort algo.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+import sorting as s
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -11,6 +12,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print("NOP")
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- sorting.py
+++ sorting.py
@@ -1,3 +1,27 @@
 class Sorting:
     def __init__(self):
         return
+
+    def quick_sort(self, arr):
+        self.quick_sort_helper(arr, 0, len(arr) - 1)
+
+    def quick_sort_helper(self, arr, s, e):
+
+        if e - s <= 0:
+            return
+
+        piv = s
+        bound = e + 1
+        for i in range(e, s, -1):
+            if arr[piv] - arr[i] < 0:
+                bound -= 1
+                arr[bound], arr[i] = arr[i], arr[bound]
+
+        bound -= 1
+        arr[bound], arr[piv] = arr[piv], arr[bound]
+        f_half_s = s
+        f_half_e = bound - 1
+        s_half_s = bound + 1
+        s_half_e = e
+        self.quick_sort(arr, f_half_s, f_half_e)
+        self.quick_sort(arr, s_half_s, s_half_e)
