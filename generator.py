b0c3926ffd1618ff6f250c5fe19ff1f43267aa46
Mostafa Tawfiq <mostafa.mohammad.twfiq@gmail.com>
2021-05-18 13:44:19 +0200

    Edited generator file.

--- generator.py
+++ generator.py
@@ -1,7 +1,5 @@
-from random import randint
 import numpy as np
 
 
 def generate_array(length):
-    arr = np.random.randint(length, size=length)
-    return arr
+    return np.random.randint(length, size=length)
